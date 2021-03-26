94f8945c2fbb41d0a2b2ec73a2b61ea88c14d08c
Boris <72406515+Cjepa02@users.noreply.github.com>
2021-03-26 21:02:24 +0100

    Update Korisnik.cs

--- Forum/Forum/Models/Korisnik.cs
+++ Forum/Forum/Models/Korisnik.cs
@@ -10,7 +10,7 @@ namespace Forum.Models
         public string ime { get; set; }
         public string prezime { get; set; }
         public string korisnickoime { get; set; }
-        public string lozinka { get; set; }s
+        public string lozinka { get; set; }
         public string tip_korisnika { get; set; } //Će metnemo enum ako bude trebalo
         public string email { get; set; }
         public int idkorisnika { get; set; }
